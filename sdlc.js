772d182bc4b2324a2e1be55b75537cbf7105f3ec
Luke Passey <ljpassey@gmail.com>
2022-09-30 10:03:03 -0600

    This commit includes the finished SDLC task.

---
+++ sdlc.js
@@ -0,0 +1,29 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis = "Analysis of requirements is the phase of the software development process where developers will determine what requirements they have for the software that they will make, what technical limitations they have, and basically defining the core components that need to be in the software."
+const Design = "This is the stage where the design process begins and the idea of what the software is going to look like and how the user will interact with it is determined."
+const Implementation = "This stage involves actually connecting the designs with their actual utilities and making it a functioning software. This can involve creating both front and back end frameworks, and the software becomes usable."
+const Testing = "Testing and integration is where developers sift through the softwares code and try to identify bugs, issues, or problems with integrations or usability. If any additional integrations need to be established through APIs or other methods, this is where that happens. Developers will usually also collect user feedback and data to assist in the process of refining the software and ironing out any kinks."
+const Maintain = "This is the stage of the development process that encompasses all the time after a software is published, or released, and involves an active monitoring of the health and performance of the software over time. As new bugs arise, developers will work to patch or fix the issues and serve to maintain the software and keep it in a healthy state."
