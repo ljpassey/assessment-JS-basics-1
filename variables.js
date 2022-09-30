f0bc89ece7fb02f45764487bbf074ef43da88bde
Luke Passey <ljpassey@gmail.com>
2022-09-30 10:07:19 -0600

    This commit includes the completed variables assignment with all questions answered.

--- variables.js
+++ variables.js
@@ -2,17 +2,17 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+const myName = "Luke"
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
+const faveNum = 3
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+const lovesCode = true
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -21,6 +21,11 @@
 */
 
 //CODE HERE
+if (faveNum === 13) {
+  console.log("lucky 13!")
+} else {
+  console.log("not lucky 13")
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +33,7 @@
 */
 
 //CODE HERE
+
+for (let i = 0; i <5; i++) {
+  console.log(faveNum)
+}
