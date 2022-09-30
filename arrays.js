eb8e4b79415f4e1180656ef834612a323ef8ce7f
Luke Passey <ljpassey@gmail.com>
2022-09-30 10:18:59 -0600

    This commit includes the finished arrays tasks with all problems completed.

--- arrays.js
+++ arrays.js
@@ -4,12 +4,15 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+let colorCopy = faveColors.slice(0,3)
+//console.log(colorCopy)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-
+colorCopy.push('blue')
+//console.log(colorCopy)
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -19,7 +22,8 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+let middleNums = numbers.slice(1, 4)
+//console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +38,14 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+
+let answers = []
+
+for (let i = 0; i <= bigOrSmallArray.length -1; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push('big')
+  } else if (bigOrSmallArray[i] <= 100) {
+    answers.push("small")
+  }
+}
+//console.log(answers)
